117abc7756571b3886f82f41a360f67264091ee7
steingartpeter <steingart.p2@gmail.com>
2023-01-22 10:38:48 +0100

    2023-01-22 Finish

--- src/app/photo-show/photo-show.component.ts
+++ src/app/photo-show/photo-show.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { PhotosService } from '../photos.service';
 
 @Component({
   selector: 'app-photo-show',
   templateUrl: './photo-show.component.html',
-  styleUrls: ['./photo-show.component.css']
+  styleUrls: ['./photo-show.component.css'],
 })
 export class PhotoShowComponent {
+  imgUrl: string = '';
+  photoCntr: number = 1;
+  constructor(private phtSrv: PhotosService) {
+    this.fetchPhoto();
+  }
 
+  updatePhoto() {
+    this.fetchPhoto();
+    this.photoCntr += 1;
+  }
+
+  fetchPhoto() {
+    this.phtSrv.getRndmPhoto().subscribe((response) => {
+      this.imgUrl = response.urls.small;
+    });
+  }
 }
--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface UnsplashRspns {
+  urls: {
+    regular: string;
+    small: string;
+  };
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotosService {
+  baseUrl: String = 'https://api.unsplash.com/photos/?client_id=';
+  apiKey: String = 'tS4irPtoTaSYEjs4SMmHBncZwuUif31inHgV-lpuoA4';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
+  getRndmPhoto() {
+    //https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
+    //currUrl: String = this.baseUrl + this.apiKey + '/random';
+    return this.http.get<UnsplashRspns>(
+      'https://api.unsplash.com/photos/random',
+      {
+        headers: {
+          Authorization:
+            'Client-ID tS4irPtoTaSYEjs4SMmHBncZwuUif31inHgV-lpuoA4',
+        },
+      }
+    );
+  }
 }
